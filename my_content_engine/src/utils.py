1728e5309a827ae4125dd8a362d422a30492f488
Mridul Tiwari <am1210975@am.iitd.ac.in>
2024-07-02 17:11:53 +0530

    update_2

--- my_content_engine/src/utils.py
+++ my_content_engine/src/utils.py
@@ -18,13 +18,13 @@ retriever = vectordb.as_retriever()
 # Initialize llama-7b language model with 
 model_id = "./llama2-7b-layla.Q5_K_S.gguf"
 # Build prompt
-template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer and don't start making up random questions and answers. Keep the answer as concise as possible. Always say "thanks for asking! BBYE!!" at the end of the answer. Once you have said, "thanks for asking" stop generating the answers and exit.
+template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer and don't start making up random questions and answers. Keep the answer as concise as possible. Always say "\nthanks for asking! BBYE!!" at the end of the answer. 
 {context}
 Question: {question}
 Answer: """
 QA_CHAIN_PROMPT = PromptTemplate.from_template(template)# Run chain
 
-n_gpu_layers = 10  # Change accordint to your GPU VRAM.
+n_gpu_layers = 15  # Change accordint to your GPU VRAM.
 n_batch = 512  # Should be between 1 and n_ctx, consider the amount of RAM of your Chip.
 
 # Make sure the model path is correct for your system!
